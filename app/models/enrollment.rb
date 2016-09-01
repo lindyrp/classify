0b308066126c8970d521471f827f65455a44cd4d
Lindy Sinclair <lindyrp@gmail.com>
2016-09-01 19:48:31 +0000

    Signup form

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,8 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :user
+
+  #example from metchess
+  #belongs_to :white_player, class_name: "User", foreign_key: "player_white_id"
+  
   belongs_to :workshop
 end
